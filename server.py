1e8bbc368ded5fafd6aa87744b546545e70864b3
Amirho3inf <mramirho3inf@gmail.com>
2024-03-10 02:23:02 +0330

    fix: change server port to 3000

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     django_app = get_wsgi_application()
 
     options = {
-        'bind': '127.0.0.1:80',
+        'bind': '127.0.0.1:3000',
         'workers': 4,
     }
     DjangoGunicornApp(django_app, options).run()
